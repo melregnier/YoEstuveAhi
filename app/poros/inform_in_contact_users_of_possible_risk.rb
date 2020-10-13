530c14729eaf1a965c50bde9445db01f3ec99679
Dago Martin de Renteria <drenteria@dc.uba.ar>
2020-10-13 20:13:54 -0300

    users_to_inform method completed in inform_in_contact_users_of-possible_risk

--- app/poros/inform_in_contact_users_of_possible_risk.rb
+++ app/poros/inform_in_contact_users_of_possible_risk.rb
@@ -16,21 +16,51 @@ class InformInContactUsersOfPossibleRisk
   private
 
   def users_to_inform
-    users_to_inform = []
-    
+    users_to_inform = Set.new
 
-    @user.user_location_histories.each do | |
-
+    @user.user_location_histories.where(date: contagious_period).each do | user_location |
+      user_location_histories = overlapping_user_location_histories(user_location).select do | overlapping_user_location |
+        MINUTES_EXPOSED_TO_BE_AT_RISK <= contact_time(user_location, overlapping_user_location).minutes
+      end
+      users_to_inform.merge(user_location_histories.map(&:user))
     end
+    users_to_inform
   end
 
   def contagious_period
-    initial_date = - PERIOD_OF_CONTAGIOUS_DAYS
-    final_date = []
-    # aca quedamos, hacer un periodo desde hasta para poder hacer el between en users_to_inform
+    initial_date = test_date - PERIOD_OF_CONTAGIOUS_DAYS
+    final_date = [test_date + PERIOD_OF_CONTAGIOUS_DAYS, Date.today].min
+    initial_date..final_date
   end
 
   def test_date
     @test_date ||= @user.covid_tests.positive.last.date
+  end
+
+  def overlapping_user_location_histories(user_location)
+    UserLocationHistory.where(location_id: user_location.location_id).where(
+      [
+        overlapping_check_in_condition, 
+        overlapping_check_out_condition, 
+        overlapping_containing_condition
+      ].join(' OR '),
+      {check_in: user_location.check_in, check_out: user_location.check_out})
+  end
+
+  def overlapping_check_in_condition
+    '(check_in >= :check_in AND check_in <= :check_out)'
+  end
+
+  def overlapping_check_out_condition
+    '(check_out >= :check_in AND check_out <= :check_out)'
+  end
+
+  def contact_time(user_location_fst, user_location_snd)
+    [user_location_fst.check_out, user_location_snd.check_out].min -
+    [user_location_fst.check_in, user_location_snd.check_in].max
+  end
+
+  def overlapping_containing_condition
+    '(check_in < :check_in AND check_out > :check_out)'
   end
 end
