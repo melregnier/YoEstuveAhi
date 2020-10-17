28f9a07c15f0b32b072045b358e5126e9a21f8d9
Dago Martin de Renteria <drenteria@dc.uba.ar>
2020-10-17 13:46:21 -0300

    home style

---
+++ app/assets/javascripts/qr_scanner.js
@@ -0,0 +1,4 @@
+function onScanSuccess(qrCodeMessage) { /** decoded message */ }
+var html5QrcodeScanner = new Html5QrcodeScanner(
+	"qr-reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
