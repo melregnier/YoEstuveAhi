25fbe6bc97afd5e65c5fff9fce47df9e6fb12fa9
Dago Martin de Renteria <drenteria@dc.uba.ar>
2020-10-24 20:00:22 -0300

    true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
