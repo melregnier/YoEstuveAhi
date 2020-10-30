33f1889625c8d5cf4046df7efe0e6d7af199c5f3
Dago Martin de Renteria <drenteria@dc.uba.ar>
2020-10-30 18:19:56 -0300

    admin view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,19 @@
 class HomeController < ApplicationController
   def index
     @user_decorator = UserDecorator.new(current_user)
+    return admin_home if current_user.admin?
+
     @location_decorator = LocationDecorator.new(@user_decorator.current_location) if @user_decorator.at_location?
     @notifications = current_user.user_notifications
     @notifications.each(&:destroy)
-    @users_by_state = User.group(:state).count
-    return admin_home if current_user.admin?
   end
 
   private
 
   def admin_home
     @locations = Location.all
+    @locations_by_capacity = Location.group(:capacity).count
+    @users_by_state = User.group(:state).count
     render 'admin_index'
   end
 end
